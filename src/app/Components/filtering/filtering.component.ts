864626c8e6160e20fe06714e214753471c84759d
SivuyileMtwetwe <sivuyilemtwetwe>
2024-06-18 18:30:19 +0200

    filltering

--- src/app/Components/filtering/filtering.component.ts
+++ src/app/Components/filtering/filtering.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PropertyService } from '../../Services/property.service';
 
 @Component({
   selector: 'app-filtering',
@@ -6,10 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './filtering.component.css'
 })
 export class FilteringComponent {
-  location: string[] = ['Nyanga', 'Samora', 'Phillippi', 'Crossroads']; 
+  locations: string[] = ['Nyanga', 'Samora', 'Phillippi', 'Crossroads']; 
   selectedLocation: string = ''; 
   filteredItems: any[] = []; // Array to hold filtered items
   allItems: any[] = []
+  filteredProperty: any[] = [];
+  property: any[] = [];
+
+constructor(  
+  private  _propertyService: PropertyService){}
 
   ngOnInit() {
     
@@ -20,13 +26,25 @@ export class FilteringComponent {
   
   }
 
+  getAllProperties(): void {
+    this._propertyService.getAllProperties().subscribe({
+      next: (data: any) => {
+        this.allItems= data;
+        this.filterByLocation = data
+      }
+    },
+  
+  )}
 
-  filterByLocation() {
-    console.log('Selected location:', this.selectedLocation);
-    if (this.selectedLocation === '') {
-      this.filteredItems = this.allItems;
-    } else {
-      this.filteredItems = this.allItems.filter(item => item.location === this.selectedLocation);
+
+  filterByLocation(location: string): any[] {  
+    const validLocation = ['Nyanga', 'Samora', 'Phillippi', 'Crossroads'];
+    if (!validLocation.includes(location)) {
+      console.log("this location is not valid" + location);
+      return [];
     }
+  
+    this.filteredItems = this.allItems.filter(item => item.location === location);
+    return this.filteredItems;
   }
 }
