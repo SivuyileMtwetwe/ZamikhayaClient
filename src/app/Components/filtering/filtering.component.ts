2ac63290765ae6baadbf271999ca4164685cd1e1
SivuyileMtwetwe <sivuyilemtwetwe>
2024-06-18 17:18:42 +0200

    filtering

--- src/app/Components/filtering/filtering.component.ts
+++ src/app/Components/filtering/filtering.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './filtering.component.css'
 })
 export class FilteringComponent {
+  location: string[] = ['Nyanga', 'Samora', 'Phillippi', 'Crossroads']; 
+  selectedLocation: string = ''; 
+  filteredItems: any[] = []; // Array to hold filtered items
+  allItems: any[] = []
 
+  ngOnInit() {
+    
+  }
+
+  onLocationChange() {
+    console.log('Selected location:', this.selectedLocation);
+  
+  }
+
+
+  filterByLocation() {
+    console.log('Selected location:', this.selectedLocation);
+    if (this.selectedLocation === '') {
+      this.filteredItems = this.allItems;
+    } else {
+      this.filteredItems = this.allItems.filter(item => item.location === this.selectedLocation);
+    }
+  }
 }
