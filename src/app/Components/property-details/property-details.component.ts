38b8c78feb67f3e314e3fe07269765c9237c5513
Abongile21 <abongiletheo00@gmail.com>
2024-07-19 23:33:03 +0200

    implement the images select and scroll on property details

--- src/app/Components/property-details/property-details.component.ts
+++ src/app/Components/property-details/property-details.component.ts
@@ -4,29 +4,30 @@ import { PropertyService } from '../../Services/Property/property.service';
 import { Location } from '@angular/common';
 import { Property } from '../../Interfaces/property';
 
-
 @Component({
   selector: 'app-property-detail',
   templateUrl: './property-details.component.html',
 })
 export class PropertyDetailComponent implements OnInit {
   property?: Property;
+  selectedImageIndex: number = 0;
+
   constructor(
     private route: ActivatedRoute,
     private propertyService: PropertyService,
-    private _location : Location,
-
+    private _location: Location,
   ) { }
 
   ngOnInit(): void {
     this.viewProperty()
   }
+
   addToFavList(property: Property): void {
     this.propertyService.addToFavlist(property)
     console.log("Added to faves")
   }
 
-  goBack(){
+  goBack() {
     this._location.back()
   }
 
@@ -42,4 +43,18 @@ export class PropertyDetailComponent implements OnInit {
     });
   }
 
+  selectImage(index: number): void {
+    this.selectedImageIndex = index;
+  }
+
+  scrollImages(direction: 'left' | 'right'): void {
+    if (!this.property || !this.property.images) return;
+
+    const totalImages = this.property.images.length;
+    if (direction === 'left') {
+      this.selectedImageIndex = (this.selectedImageIndex - 1 + totalImages) % totalImages;
+    } else {
+      this.selectedImageIndex = (this.selectedImageIndex + 1) % totalImages;
+    }
+  }
 }
